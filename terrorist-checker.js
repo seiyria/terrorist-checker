0e3bdad717ab9dc8fb15a5ef77a9d79ee91ef9b9
Evan Shimoniak <eshimoniak@gmail.com>
2015-12-17 21:03:10 -0800

    Semicolons

--- terrorist-checker.js
+++ terrorist-checker.js
@@ -1,9 +1,9 @@
 var country = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
 
-var america = ["america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"]
+var america = ["america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"];
 
 //If country is a spelling of America
-if (america.indexOf(country) > -1); {
+if (america.indexOf(country) > -1) {
   alert('OK. You can use this website normally.');
 } else {
   alert('You might be a terrorist')
