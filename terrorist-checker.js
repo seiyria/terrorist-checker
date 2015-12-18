5333dd50b66e927c91d526b9205c406e07d3bc79
Evan Shimoniak <eshimoniak@gmail.com>
2015-12-17 21:38:30 -0800

    Added alternate questions, increased extensibility

--- terrorist-checker.js
+++ terrorist-checker.js
@@ -1,12 +1,35 @@
-var country = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
+var answer = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
 
-var america = ["america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"];
+var currQnum = 0;
 
-//If country is a spelling of America
-if (america.indexOf(country) > -1) {
-  alert('OK. You can use this website normally.');
-} else {
-  alert('You might be a terrorist')
-  document.write('<img src=\"https://placekitten.com/g/500/200\" />');
-  document.write('<!--');
+
+var questions = {
+  "Enter the name of your birth country":[
+    "america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"
+  ],
+  "Are you muslim?":[
+    "america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"
+  ],
+  "How brown are you?":[
+    "white", "caucasian", "not brown", "pale", "porcelin", "sour cream"
+  ]
+};
+
+var keys = Object.keys(questions);
+var newQuestion = ["i don't want to answer", "prefer not to answer", "i'd like another question"];
+
+while (true) {
+  if (questions[keys[currQnum]].indexOf(answer) > -1) {
+    alert('OK. You can use this website normally.');
+  } else if (newQuestion.indexOf(answer) > -1) {
+    currQnum++;
+    if (currQnum >= keys.size()) {
+      currQnum = 0;
+    }
+  } else {
+    alert('You might be a terrorist')
+    document.write('<img src=\"https://placekitten.com/g/500/200\" />');
+    document.write('<!--');
+    break;
+  }
 }
