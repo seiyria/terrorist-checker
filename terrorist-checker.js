ea76dcdf3d57d25c73276539baa5084e039f6ddc
Evan Shimoniak <eshimoniak@gmail.com>
2015-12-17 17:10:14 -0800

    Beautified, added more #America spellings

--- terrorist-checker.js
+++ terrorist-checker.js
@@ -1,5 +1,9 @@
-country = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
-if(country == "america" || country == "'merica" || country == "usa" || country == "us" || country == "states" || country == "'murica") {
+var country = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
+
+var america = ["america", "'merica", "usa", "us", "states", "'murica", "merica", "murica"]
+
+//If country is a spelling of America
+if (america.indexOf(country) > -1); {
   alert('OK. You can use this website normally.');
 } else {
   alert('You might be a terrorist')
