cf8b70f2d5a38be22511fd752a15b35a0b9c8545
Jack Kingsman <jack.kingsman@gmail.com>
2015-12-17 19:59:08 -0800

    If don't check identities, the terrorists win.

--- terrorist-checker.js
+++ terrorist-checker.js
@@ -1,5 +1,5 @@
 country = prompt("Enter the name of your birth country", "somewhere except america").toLowerCase();
-if(country == "america" || country == "'merica" || country == "usa" || country == "us" || country == "states" || country == "'murica") {
+if(country === "america" || country === "'merica" || country === "usa" || country === "us" || country === "states" || country === "'murica") {
   alert('OK. You can use this website normally.');
 } else {
   alert('You might be a terrorist')
