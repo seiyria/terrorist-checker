7bf3580dec7112d5c2ddb7409d70147cd439cb3a
Gytis Daujotas <gytdau@gmail.com>
2015-12-19 20:32:33 +0000

    Added alternative error page
    This is another take on the error page.

--- terrorist-checker.js
+++ terrorist-checker.js
@@ -3,6 +3,8 @@ if(country === "america" || country === "'merica" || country === "usa" || countr
   alert('OK. You can use this website normally.');
 } else {
   alert('You might be a terrorist')
-  document.write('<img src=\"https://placekitten.com/g/500/200\" />');
+  document.write('<h1>Error 406: You are a terrorist</h1>')
+  document.write('<h3>The server has rejected you because it has detected terrorist behaviours.</h3>')
+  document.write('<ul><li>Double check your identity</li><li>Search your house for any bombs</li><li>Think really hard to remember if you&apos;ve been planning any terrorist regimes</li><li>Check your Internet connection</li></ul>')
   document.write('<!--');
 }
