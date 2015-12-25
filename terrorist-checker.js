fd3d09faf6212ec2ecebf2aa9ee1879709938e2a
Rab Rennie <RENNIERAB@gmail.com>
2015-12-25 06:42:05 +0000

    Fixed all bugs
    Merry Christmas

--- terrorist-checker.js
+++
@@ -1,8 +0,0 @@
-country = prompt("Are you a terrorist? Type 'no' to answer no, or anything else to answer yes", "yes").toLowerCase();
-if(answer == "no") {
-  alert('OK. You can use this website normally.');
-} else {
-  alert('You might be a terrorist')
-  document.write('<img src=\"https://placekitten.com/g/500/200\" />');
-  document.write('<!--');
-}
